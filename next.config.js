76c93b03e752dc43f14cd1ef8f4f202d2fa429b6
vijoh01cd <vijoh01@gmail.com>
2024-06-30 22:17:32 +0200

    init

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'src/build',
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*', // Match any /api/ requests
-          destination: 'http://0.0.0.0:3000/api/:path*', // Replace with your backend API URL
-        },
-      ];
-    },
-  };
+};
   
   module.exports = nextConfig;
